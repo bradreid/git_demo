564d713d66a4722b7e80150da37ed6d7009b279d
Brad Reid <bradreid20@gmail.com>
2012-10-30 20:48:09 -0700

    fix tests

--- test/view/welcome/index_test.rb
+++ test/view/welcome/index_test.rb
@@ -2,8 +2,14 @@ require_relative '../../test_helper'
 
 module Welcome
   class IndexTest < ActionView::TestCase
+
+    setup do
+      ActionController::Base.prepend_view_path 'app/views/welcome/'
+      render template: 'index'
+    end
+
     should 'have a heading' do
-      assert_select 'h3', 'welcome'
+      assert_select 'h3', 'Welcome'
     end
   end
 end
