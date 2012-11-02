cb9a5611ae5198c78072045342c6b705ac171d92
Brad Reid <bradreid20@gmail.com>
2012-11-02 11:57:53 -0700

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -48,6 +48,7 @@ GitDemo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+   resources :users
    root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
